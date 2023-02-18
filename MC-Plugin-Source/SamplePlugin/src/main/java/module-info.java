2104ad40672b0f57aa422568f305e2e998be4481
piro56 <69094266+piro56@users.noreply.github.com>
2023-02-18 00:21:52 -0500

    Removing Sample Plugin

--- MC-Plugin-Source/SamplePlugin/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.sampleplugin {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.sampleplugin to javafx.fxml;
-    exports com.example.sampleplugin;
-}
