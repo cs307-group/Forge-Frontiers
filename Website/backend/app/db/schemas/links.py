329aa4f3b8ba3fb270ea1dadb83df46f4a651667
Hydrophobefireman <bhavesh.pareek36@gmail.com>
2023-03-02 12:28:45 -0500

    feat: links table and begin link impl

---
+++ Website/backend/app/db/schemas/links.py
@@ -0,0 +1,32 @@
+from secrets import token_urlsafe
+from time import time
+
+from sqlalchemy.dialects.postgresql import TEXT
+from sqlalchemy import func
+
+from ..base import db
+
+
+class Links(db.Model):
+    # pylint: disable=E1101
+    link_id: str = db.Column(TEXT, unique=True, nullable=False, primary_key=True)
+    player_uuid: str = db.Column(TEXT, unique=True, nullable=False)
+    link_code: str = db.Column(TEXT, unique=True, nullable=False)
+    bool_used: bool = db.Column(db.Boolean, default=False)
+
+    # pylint: enable=E1101
+
+    def __init__(self, player_uuid: str = None, link_code: str = None, bool_used=False):
+        self.link_id = token_urlsafe(20)
+        self.player_uuid = player_uuid
+        self.link_code = link_code
+        self.bool_used = bool_used
+
+    @property
+    def as_json(self):
+        return {
+            "link_id": self.link_id,
+            "player_uuid": self.player_uuid,
+            "link_code": self.link_code,
+            "bool_used": self.bool_used,
+        }
---
+++ Website/frontend/src/config.ts
@@ -0,0 +1,3 @@
+export const config = {
+  REQUIRE_ACCOUNT_LINK_TO_PROCEED: true,
+};
