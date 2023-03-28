0d0d2b0450c56135d20a55fedcc41e846f30c49a
Joseph Miller <millerjoseph449@gmail.com>
2023-03-28 15:09:51 -0400

    Continued working on implementing custom mobs. Derived a way for passive mobs to attack players
    Planning to work on a way to get a mob to correctly follow a player.
    
    Custom Items are still broken (i.e. armorchange event is broken)

--- MC-Plugin-Source/ForgeFrontier/src/main/java/com/forgefrontier/forgefrontier/mobs/chickens/CustomChickenEntity.java
+++ MC-Plugin-Source/ForgeFrontier/src/main/java/com/forgefrontier/forgefrontier/mobs/chickens/CustomChickenEntity.java
@@ -1,26 +1,47 @@
 package com.forgefrontier.forgefrontier.mobs.chickens;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.attributes.*;
-import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.behavior.FollowTemptation;
+import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.monster.PatrollingMonster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.bukkit.Bukkit;
 
 public class CustomChickenEntity extends Chicken {
+
+    private long lastDamageTime = 0;
+
     public CustomChickenEntity(EntityType<? extends Chicken> entitytypes, Level world) {
         super(entitytypes, world);
-
-        /*
-        AttributeMap map = this.getAttributes();
-        AttributeInstance attackAttribute = new AttributeInstance(map, Attributes.ATTACK_DAMAGE, 10);
-         */
     }
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new NearestAttackableTargetGoal<Player>(this, Player.class, 100, true, true, null));
-        this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1, true));
+        this.goalSelector.addGoal(2, new MoveTowardsTargetGoal(this, 1.0, 10));
+        this.goalSelector.addGoal(2, new FollowMobGoal(this, 1.0, 10, 1));
+        //this.goalSelector.addGoal(1, new RandomStrollGoal(this, 1.0, 25));
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+
+        Player nearestPlayer = this.getLevel().getNearestPlayer(this, 15.0);
+        if (nearestPlayer != null) {
+            this.setTarget(nearestPlayer);
+            org.bukkit.entity.Player spigotPlayer = Bukkit.getPlayer(nearestPlayer.getUUID());
+            if (spigotPlayer != null && System.currentTimeMillis() - lastDamageTime > 1000 &&
+                    nearestPlayer.distanceTo(this) <= 1) {
+                spigotPlayer.damage(1);
+                lastDamageTime = System.currentTimeMillis();
+            }
+        } else {
+            this.setTarget(null);
+        }
     }
 }
